4497c5a2da237b99ecc5ecd4e0d04f3d9f976a6c
Siddhi <siddhitiwari0827@gmail.com>
2025-04-23 20:31:22 +0530

    Temp

---
+++ script.js
@@ -0,0 +1,24 @@
+import { createClient } from 'pexels';
+import dotenv from 'dotenv';
+import fs from 'fs';
+
+
+dotenv.config();
+
+const client = createClient(process.env.API_KEY);
+const query = 'house';
+
+async function searchPhotos() {
+    try {
+        const response = await client.photos.search({ query, per_page: 232320 });
+        const arr =[]
+        for (const photo of response.photos){
+            arr.push(photo.url)
+        }
+        fs.writeFileSync('ImageUrl.json', JSON.stringify(arr, null, 2));
+    } catch (error) {
+        console.error('Error fetching photos:', error);
+    }
+}
+
+searchPhotos();
